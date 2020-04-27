cc3b1094b03032dadef92a5e7b3e948e7ad36aa0
Santhosh K S <santhoshachar08@gmail.com>
2020-04-27 12:16:54 +0200

    partial factories

--- v6/upload.py
+++ v6/upload.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, request, send_from_directory, flash, session
+from flask import Flask, render_template, \
+  request, send_from_directory, flash, session
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 
-app = Flask(__name__)
-app.secret_key = "super secret key"
+#https://hackersandslackers.com/flask-application-factory/
 
-photos = UploadSet('photos', IMAGES)
 
-out_path = '/tmp/images/'
-app.config['UPLOADED_PHOTOS_DEST'] = out_path
-configure_uploads(app, photos)
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = "super secret key"
+    out_path = '/tmp/images/'
+    app.config['UPLOADED_PHOTOS_DEST'] = out_path
+    photos = UploadSet('photos', IMAGES)
+    configure_uploads(app, photos)
+    return app, photos
+
+app, photos = create_app()
+
 
 class DataObj():
     pass
@@ -37,7 +44,11 @@ def upload():
             session["filename"] = filename
             obj.is_image_display = True
         else:
-            flash(f"File : {ft.filename} is not an image file")
+            if ft.filename:
+                msg = f"{ft.filename} is not an image file"
+            else:
+                msg = "Please select an image file"
+            flash(msg)
         return render_template('upload.html', obj=obj)
     return render_template('upload.html', obj=obj)
 
